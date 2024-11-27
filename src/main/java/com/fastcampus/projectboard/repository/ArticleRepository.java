38e7c051cefa8ffb10ede55f398459e46e9320c2
HolyPaper <bardoje123@gmail.com>
2024-11-28 03:29:33 +0900

    #2 - Querydsl 오류 수정

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 @RepositoryRestResource
 public interface ArticleRepository extends
