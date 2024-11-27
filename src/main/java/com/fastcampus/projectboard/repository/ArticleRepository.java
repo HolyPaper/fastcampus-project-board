7334c0f3bac718236df1d93acfcf15a654ff44e4
HolyPaper <bardoje123@gmail.com>
2024-11-28 03:27:28 +0900

    Revert "#2 - Querydsl 오류 수정"
    This reverts commit e551439b3740584571138b4efe1b3537306cc850.

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -8,7 +8,6 @@ import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
