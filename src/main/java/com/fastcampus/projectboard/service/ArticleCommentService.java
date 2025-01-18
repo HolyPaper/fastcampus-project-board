d03aab1368fe272786a4626525ab5943ccd41508
HolyPaper <bardoje123@gmail.com>
2025-01-19 01:23:42 +0900

    #21 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    강의에서 보여주지 않은 나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
+++ src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
@@ -10,18 +10,25 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @RequiredArgsConstructor
-@Service
 @Transactional
+@Service
 public class ArticleCommentService {
 
-    private ArticleCommentRepository articleCommentRepository;
-    private ArticleRepository articleRepository;
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
 
     @Transactional(readOnly = true)
-    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+    public List<ArticleCommentDto> searchArticleComments(Long articleId) {
         return List.of();
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
     }
+
+    public void updateArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
+    }
+
 }
